11d37e6805db6393d2cdac1f94ce0873a9ec354e
Justin <16kimj97@gmail.com>
2023-12-20 10:48:10 -0800

    first

--- phase-1.js
+++ phase-1.js
@@ -27,12 +27,12 @@ function liftWeights() {
 
 // refactor this function to handle Promises using async/await instead of
   // .then and .catch
-function workout() {
-  stretch()
-    .then(runOnTreadmill)
-    .then(liftWeights)
-    .then(() => console.log("done working out"))
-    .catch((err) => console.log(err));
+async function workout() {
+    await stretch()
+    await runOnTreadmill()
+    await liftWeights()
+    console.log("done working out")
+    // .await((err) => console.log(err));
 }
 
 /* ============================ TEST YOUR CODE ============================
